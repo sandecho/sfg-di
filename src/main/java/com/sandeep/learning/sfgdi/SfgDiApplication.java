22a9cca5b783b96a28884e1270f4819eeab946a8
sandecho <tssandeepkumarchoudhary@gmail.com>
2022-10-12 18:55:38 +0530

    Spring Context

--- src/main/java/com/sandeep/learning/sfgdi/SfgDiApplication.java
+++ src/main/java/com/sandeep/learning/sfgdi/SfgDiApplication.java
@@ -1,13 +1,23 @@
 package com.sandeep.learning.sfgdi;
 
+import com.sandeep.learning.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+        MyController controller = (MyController) context.getBean("myController");
+
+        String greetings = controller.sayHello();
+
+        System.out.println(greetings);
+
+    }
 
 }
